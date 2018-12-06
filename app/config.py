252ebfcc44a61db766a4ed79057eacf6ef2eca3b
Ubuntu <ubuntu@ip-172-31-31-226.ap-northeast-2.compute.internal>
2018-12-06 03:37:19 +0000

    1

--- app/config.py
+++ app/config.py
@@ -4,12 +4,12 @@ TIMEZONE = 'Asia/Seoul'
 
 SECRET_KEY = 'cs409'
 
-DEBUG = True
+DEBUG = False
 
 LOG_LEVEL = logging.DEBUG
 LOG_FILENAME = 'activity.log'
 LOG_MAXBYTES = 1024
 LOG_BACKUPS = 2
 
-ASSETS_DIR = 'assets'
+ASSETS_DIR = 'app/assets'
 MAXLEN = 49
